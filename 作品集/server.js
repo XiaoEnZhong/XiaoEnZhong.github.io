cb48f33b1a2e286925348c1e2d27bd167e4cc093
XiaoEnZhong <62697271+XiaoEnZhong@users.noreply.github.com>
2021-07-31 21:52:51 +0800

    Create server.js

---
+++ 作品集/server.js
@@ -0,0 +1,89 @@
+const http = require("http");
+const fs = require("fs");
+const qs = require("querstring");
+
+const port = 3000;
+const ip = "127.0.0.1";
+
+const sendResponse = (filename, statusCode, response) => {
+    fs.readFile(`./html/${filename}` , (error, data) => {
+        if (error) {
+            response.statusCode = 500;
+            response.setHeader("Content-Type", "text/plain");
+            response.end("sorry, internal error");
+        }
+        else {
+            response.statusCode = statusCode;
+            response.setHeader("Contant-Type", "text/html");
+            response.end(data);
+        }
+    });
+};
+
+//request = 請求object
+//response = 響應object
+const server = http.createServer((request, response) => {
+    //console.log(request.url, request.method);
+    const method = request.method;
+    let url = request.url;
+
+    if (method === "GET") {
+        const requestUrl = new URL(url, `http://${ip}:${port}`);
+        //console.log(requestUrl);
+        //console.log(requestUrl.searchParams.get("lang"));
+        url = requestUrl.pathname;
+        const lang = requestUrl.searchParams.get("lang");
+        let selector;
+
+        if (lang === null || lang === "en") {
+            selector = "";
+        } else if (lang === "zh") {
+            selector = "-zh";
+        } else {
+            selector = "";
+        }
+
+        if (url === "/") {
+            sendResponse(`index${selector}.html`, 200, response);
+        } else if (url === "/about.html") {
+            sendResponse(`about${selector}.html`, 200, response);
+        } else if (url === "/login.html") {
+            sendResponse(`login${selector}.html`, 200, response);
+        } else if (url === "/login-success.html") {
+            sendResponse(`login-success${selector}.html`, 200, response);
+        } else if (url === "/login-fail.html") {
+            sendResponse(`login-fail${selector}.html`, 200, response);
+        }
+     else {
+        sendResponse(`404${selector}.html`, 404, response);
+    }
+} else {
+    if (url === "/process-login") {
+        let body = [];
+
+        request.on("data", (chunk) =>{
+            body.push(chunk);
+        } );
+        request.on("end", () =>{ 
+            body = Buffer.concat(body).toString();
+            body = qs.parse(body);
+            console.log(body);
+
+            if (body.username === "enzhong" && body.password === "enzhong") {
+                response.statusCode = 301;
+                response.setHeader("Location","/login-success.html");
+            } else {
+                response.statusCode = 301;
+                response.setHeader("Location","/login-fail.html");
+            }
+            response.end();
+        });
+    }
+} 
+  
+});
+//1.端口(port) 2.ip地址 3.回調函數(callback)
+
+server.listen(port, ip, () => {
+  console.log(`server is running at http://${ip}:${port}`);
+});
